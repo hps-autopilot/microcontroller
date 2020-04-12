6246d6394c164495ca8d2801a1489a05e8976c37
JUNXIANG CHEN <jxchen16@uw.edu>
2020-04-12 01:20:14 -0700

    test

--- capstone project/hps_auto_pilot/hps_auto_pilot.ino
+++ capstone project/hps_auto_pilot/hps_auto_pilot.ino
@@ -3,11 +3,26 @@
 #include <Arduino_HTS221.h>
 #include <Arduino_APDS9960.h>
 #include <ArduinoBLE.h>
+//#include <stdlib.h>
 //#include <AudioSound.h>
 //#include <PDM.h>
 
 #include <stdio.h>
 unsigned long timer;
+int str_size = 256;
+BLEService sensorService("19B10000-E8F2-537E-4F6C-D104768A1214"); // BLE LED Service
+
+// BLE LED Switch Characteristic - custom 128-bit UUID, read and writable by central
+BLECharacteristic accelerometer("19B10000-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite, str_size, "Accelerometer");
+BLECharacteristic gyroscope("19B10000-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite, str_size, "Gyroscope");
+//BLECharacteristic acc_y("19B10000-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite, "");
+//BLECharacteristic acc_z("19B10000-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite, "");
+//
+//BLECharacteristic gyro_x("19B10000-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite, "");
+//BLECharacteristic gyro_y("19B10000-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite, "");
+//BLECharacteristic gyro_z("19B10000-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite, "");
+
+
 
 void setup() {
   if (!BLE.begin()) {
@@ -15,16 +30,32 @@ void setup() {
 
     while (1);
   }
-  BLE.setLocalName("Arduino Sensors");
+  BLE.setLocalName("Adruino Sensors");
   BLE.setDeviceName("Arduino Sensors");
+
+  BLE.setAdvertisedService(sensorService);
+
+  // add the characteristic to the service
+  sensorService.addCharacteristic(accelerometer);
+  sensorService.addCharacteristic(gyroscope);
+//  sensorService.addCharacteristic(acc_z);
+//  sensorService.addCharacteristic(gyro_roll);
+//  sensorService.addCharacteristic(gyro_pitch);
+//  sensorService.addCharacteristic(gyro_yaw);
+
+  // add service
+  BLE.addService(sensorService);
+
+  // start advertising
   BLE.advertise();
 
+  Serial.println("Arduino NANO 33 BLE Sensors");
 
   // put your setup code here, to run once:
   //initialization
   Serial.begin(9600);
   if (!IMU.begin()) {
-    Serial.println("Failed to initialize IMU!");
+    Serial.println("Failed to initialize IMU sensor!");
     while (1);
   }
 
@@ -48,67 +79,79 @@ void setup() {
 }
 
 void loop() {
-  delay(1000);
-  while (BLE.connected()) {
-    Serial.println("Connection established");
+  String acc_data, gyro_data;
+  float x, y, z;
+  float yaw, pitch, roll;
+  char acc_char[str_size];
+  char gyro_char[str_size];
+  BLEDevice central = BLE.central();
+  //
+  // if a central is connected to peripheral:
+  if (central) {
+    while (central.connected()) {
+      delay(1000);
+      Serial.println("connect successfully");
+      //IMU
+      if (IMU.accelerationAvailable()) {
+        IMU.readAcceleration(x, y, z);
+        acc_data = String(x, 2) + "," + String(y, 2) + "," + String(z, 2);
+        acc_data.toCharArray(acc_char, str_size);
+        accelerometer.writeValue(acc_char);
 
-    // put your main code here, to run repeatedly:
-    delay(1000);
-    timer = millis();
-    Serial.println(timer);
-//
-//    //pressure sensor
-//    float pressure = BARO.readPressure();
-//    Serial.print("Pressure = ");
-//    Serial.print(pressure);
-//    Serial.println(" kPa");
+      }
+  
 
-    //IMU
-    float x, y, z;
-    if (IMU.accelerationAvailable()) {
-      IMU.readAcceleration(x, y, z);
-      //x is the long side angle , y is the short side angle, z is surface angle
-      Serial.print(F("x: "));
-      Serial.print(int(x * 100) / 100.0);
-      Serial.print(" \t");
-      Serial.print(F("y: "));
-      Serial.print(int(y * 100) / 100.0);
-      Serial.print(" \t");
-      Serial.print(F("z: "));
-      Serial.println(int(z * 100) / 100.0);
+      if (IMU.gyroscopeAvailable()) {
+        IMU.readGyroscope(roll, pitch, yaw);
+        //gyro_data = String(roll + ", " + pitch + ", " + yaw);
+        //gyro_data = String(roll) + "," + String(pitch) + "," + String(yaw);
+        //gyro_data.toCharArray(gyro_char, str_size);
+        //gyroscope.writeValue(gyro_char);
+      }
+
     }
 
-    float yaw, pitch, roll;
 
-    if (IMU.gyroscopeAvailable()) {
-      IMU.readGyroscope(roll, pitch, yaw);
+    // when the central disconnects, print it out:
+    Serial.print(F("Disconnected from central: "));
+    Serial.println(central.address());
+  }
 
-      Serial.print(F("roll: "));
-      Serial.print(roll);
-      Serial.print(" \t");
-      Serial.print(F("pitch: "));
-      Serial.print(pitch);
-      Serial.print(" \t");
-      Serial.print(F("yaw: "));
-      Serial.println(yaw);
-    }
+  //  delay(1000);
+  //  while (BLE.connected()) {
+  //    Serial.println("Connection established");
+  //
+  //    // put your main code here, to run repeatedly:
+  //    delay(1000);
+  //    timer = millis();
+  //    Serial.println(timer);
+  ////
+  ////    //pressure sensor
+  ////    float pressure = BARO.readPressure();
+  ////    Serial.print("Pressure = ");
+  ////    Serial.print(pressure);
+  ////    Serial.println(" kPa");
+  //
 
-    float mag_x, mag_y, mag_z;
+  //
 
-    if (IMU.magneticFieldAvailable()) {
-
-      IMU.readMagneticField(mag_x, mag_y, mag_z);
-      Serial.print(F("x: ")); \
-      Serial.print(mag_x);
-      Serial.print(" \t");
-      Serial.print(F("y: "));
-      Serial.print(mag_y);
-      Serial.print(" \t");
-      Serial.print(F("z: "));
-      Serial.print(mag_z);
-      Serial.println("uT");
-    }
-  } 
+  //
+  //    float mag_x, mag_y, mag_z;
+  //
+  //    if (IMU.magneticFieldAvailable()) {
+  //
+  //      IMU.readMagneticField(mag_x, mag_y, mag_z);
+  //      Serial.print(F("mag x: ")); \
+  //      Serial.print(mag_x);
+  //      Serial.print(" \t");
+  //      Serial.print(F("mag y: "));
+  //      Serial.print(mag_y);
+  //      Serial.print(" \t");
+  //      Serial.print(F("mag z: "));
+  //      Serial.print(mag_z);
+  //      Serial.println("uT");
+  //    }
+  //  }
   /*
     //Gesture sensor
     if (APDS.gestureAvailable()) {
@@ -189,7 +232,7 @@ void loop() {
 
     //…
 
-    // query the number of bytes available
+    // query the number  of bytes available
     int bytesAvailable = PDM.available();
 
     // read into the sample buffer
