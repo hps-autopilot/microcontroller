28c8491147e93e40553d1ea21d10c28f34c84a6a
Chase Hunter <61028664+Chase735@users.noreply.github.com>
2020-04-15 12:18:34 -0700

    SD card version
    SD card capable file. Will not function if SD storage is not connected.

---
+++ capstone project/hps_auto_pilot_SD/hps_auto_pilot_SD.ino
@@ -0,0 +1,127 @@
+#include <Arduino_LSM9DS1.h>  //IMU
+#include <Arduino_LPS22HB.h>  //Pressure Sensor
+#include <Arduino_HTS221.h>   //Humidity & Temperature Sensor
+#include <ArduinoBLE.h>       //BLE Library
+#include <SPI.h>              //SPI interface
+#include <SD.h>               //SD card management
+
+#include <stdio.h>
+unsigned long timer;
+int str_size = 256;
+BLEService sensorService("19B10000-E8F2-537E-4F6C-D104768A1214"); // BLE sensor Service
+
+// BLE LED Switch Characteristic - custom 128-bit UUID, read and writable by central
+// two characteristics, one for accelerometer and one for gyroscope
+BLECharacteristic accelerometer("19B10000-E8F2-537E-4F6C-D104768A1215", BLENotify, "Accelerometer: (x_placeholder, y_placeholder, z_placeholder)");
+BLECharacteristic gyroscope("19B10000-E8F2-537E-4F6C-D104768A1216", BLENotify, "Gyroscope: (roll_placeholder, pitch_placeholder, yaw_placeholder)");
+
+File myFile;
+
+void setup() {
+  Serial.begin(9600);
+  
+  if (!IMU.begin()) {
+      Serial.println("Failed to initialize IMU!");
+      while (1);
+  }
+  if (!BARO.begin()) {
+    Serial.println("Failed to initialize pressure sensor!");
+    while (1);
+  }
+  if (!HTS.begin()) {
+    Serial.println("Failed to initialize humidity temperature sensor!");
+    while (1);
+  }
+  if (!BLE.begin()) {
+    Serial.println("starting BLE failed!");
+
+    while (1);
+  }
+  // initialize SD card connected to pin 10
+  if (!SD.begin(10)) {
+    Serial.println("initialization failed!");
+    while (1);
+  }
+
+  //set name to for the board 
+  BLE.setLocalName("Adruino Sensors");
+  BLE.setDeviceName("Arduino Sensors");
+
+  BLE.setAdvertisedService(sensorService);
+
+  // add the characteristic to the service
+  sensorService.addCharacteristic(accelerometer);
+  sensorService.addCharacteristic(gyroscope);
+
+  // add service
+  BLE.addService(sensorService);
+
+  // start advertising
+  BLE.advertise();
+
+  Serial.println("Arduino NANO 33 BLE Sensors");
+}
+
+void loop() {
+  String acc_data, gyro_data;
+  float x, y, z;
+  float yaw, pitch, roll;
+  char acc_char[str_size];
+  char gyro_char[str_size];
+  BLEDevice central = BLE.central();
+  
+  // if a central is connected to peripheral:
+  if (central) {
+  
+    // open text file named 'data.txt' for write
+    // if file does not exist, it is created
+    myFile = SD.open("data.txt", FILE_WRITE);
+  
+    // check if file is opened without issue
+    if (myFile) {
+      // print header between data sets
+      myFile.println("============");
+      myFile.println("NEW DATA SET");
+      myFile.println("============");
+      myFile.println("x,y,z,roll,pitch,yaw");
+      Serial.println("write done");
+    } else {
+      // if the file didn't open, print an error:
+      Serial.println("error opening data file");
+    }
+    
+    while (central.connected()) {
+      delay(1000);
+      Serial.println(sensorService.characteristicCount());
+
+      Serial.println("connect successfully");
+      
+      //IMU
+      if (IMU.accelerationAvailable()) {
+        IMU.readAcceleration(x, y, z);
+        
+        //String concatenation 
+        acc_data = String("Accelerometer: (") + String(x) + "," + String(y) + "," + String(z) + String(")");
+		//Print accelerometer data to SD card
+        myFile.print(String(x) + "," + String(y) + "," + String(z) + ",");
+        acc_data.toCharArray(acc_char, str_size);
+        accelerometer.writeValue(acc_char);
+      }
+  
+      if (IMU.gyroscopeAvailable()) {
+        IMU.readGyroscope(roll, pitch, yaw);
+        gyro_data = String("Gyroscope: (") + String(roll) + "," + String(pitch) + "," + String(yaw) + String(")");
+		//Print gyroscope data to SD card
+        myFile.println(String(roll) + "," + String(pitch) + "," + String(yaw));
+        gyro_data.toCharArray(gyro_char, str_size);
+        gyroscope.writeValue(gyro_char);
+      }
+    }
+
+    // when the central disconnects, print it out:
+    Serial.print(F("Disconnected from central: "));
+    Serial.println(central.address());
+  }
+  myFile.close();
+
+}
